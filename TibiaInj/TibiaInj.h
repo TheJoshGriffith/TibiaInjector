c4fd2cf7de218b5dc794a7381dcb1a9df620f707
XtrmJosh <josh@joshgriffith.co.uk>
2013-12-03 20:13:28 +0000

    Removed namespace from header

--- TibiaInj/TibiaInj.h
+++ TibiaInj/TibiaInj.h
@@ -5,18 +5,18 @@
 	
 	This file is part of TibiaInjector.
 
-    Foobar is free software: you can redistribute it and/or modify
+    TibiaInjector is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    TibiaInjector is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with TibiaInjector.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <thread>
@@ -41,8 +41,6 @@
 #ifndef WALKINGOFFSET
 #define WALKINGOFFSET 0x050
 #endif
-
-using namespace std;
 
 class TibiaInj
 {
